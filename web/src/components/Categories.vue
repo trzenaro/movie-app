c2fd53dde467f0fc65caf5f2e04d37000095d644
Thiago Zenaro <thiago.zenaro@i-value.com.br>
2019-12-10 23:23:30 -0300

    add basic vue view for login and homepage

---
+++ web/src/components/Categories.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>CATEGORIAS!!</div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ web/src/components/Main.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-list-item :key="item.text" link :to="item.link">
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">Movie App</span>
+      </v-toolbar-title>
+      <v-spacer />
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon large v-on="on">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="(item, index) in [{title:'Sair'}]" link :key="index" @click="logout">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  methods: {
+    logout: function() {
+      this.$router.push("/login");
+    }
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { icon: "mdi-movie", text: "Itens", link: "/items" },
+      { icon: "mdi-file-document", text: "Categorias", link: "/categories" },
+      { icon: "mdi-contacts", text: "Usuários", link: "/users" }
+    ]
+  })
+};
+</script>
