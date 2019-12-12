64fff553e7543118dcae668a86480d9c080a846d
Thiago Zenaro <thiago.zenaro@i-value.com.br>
2019-12-12 17:55:46 -0300

    (back/front) add docker file to projects and setup docker-compose.yml (front) fix some layout alignments

--- api/src/domain/models/item/item-model.js
+++ api/src/domain/models/item/item-model.js
@@ -6,6 +6,7 @@ const ItemSchema = new Schema(
     {
         name: { type: String, required: true, text: true },
         category: { type: Schema.Types.ObjectId, ref: 'Category', required: true, index: true },
+        imageUrl: { type: String },
         isActive: { type: Boolean }
     },
     {
